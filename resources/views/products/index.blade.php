06bad0958b3e25b4713d25b1c0e0eadcadce04c9
Harkirat <harkiratsingh67895@gmail.com>
2026-04-16 18:31:25 +0530

    asd

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1484,9 +1484,7 @@
                 @elseif(isset($category))
                     {{ $category->name }}
                 @else
-                Onkar <span style="color: var(--g-bg, #facc15) !important;">Enterprises</span>       
-                @endif
-            </h1>
+                Onkar <span style="color: var(--g-accent, #facc15) !important;">Enterprises</span>            </h1>
 
             <div class="g-hero-eyebrow">
             Distributor for automobile spare parts
